da0fba9104fa1eadde46520abd843297eaa5819d
DaryaKirienko <83349263+DaryaKirienko@users.noreply.github.com>
2021-07-30 12:22:07 +0300

    Сверстали страницу с объявлением, адаптировали

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+  },
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
+
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
